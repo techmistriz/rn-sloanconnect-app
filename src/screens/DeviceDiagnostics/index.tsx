2d364fe54f3e94f63434745e84bd689cb3b59f82
Deepak Maurya <deepakmaurya@hotmail.com>
2024-07-18 07:32:31 +0530

    Fixed the bottom white line issue on diagnostic screen

--- src/screens/DeviceDiagnostics/index.tsx
+++ src/screens/DeviceDiagnostics/index.tsx
@@ -468,87 +468,83 @@ const Index = ({navigation, route}: any) => {
   };
 
   return (
-    <AppContainer
-      scroll={false}
-      scrollViewStyle={{}}
-      backgroundType="solid"
-      loading={loading}>
-      <Wrap autoMargin={false} style={styles.mainContainer}>
-        <Image
-          // @ts-ignore
-          source={getImgSource(Images?.activateFaucet)}
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            opacity: 0.7,
-          }}
-          resizeMode="cover"
-          // blurRadius={1}
-        />
-        <Header hasBackButton headerBackgroundType="transparent" />
-        <Wrap autoMargin={false} style={styles.sectionContainer}>
-          <Wrap autoMargin={false} style={styles.section1}>
-            <Wrap autoMargin={false}>
-              <Typography
-                size={22}
-                text={`Diagnostics`}
-                style={{textAlign: 'center', marginTop: 0, lineHeight: 25}}
-                color={Theme.colors.white}
-                ff={Theme.fonts.ThemeFontMedium}
-              />
-              <Typography
-                size={14}
-                text={`Place your hand in front of the \nfaucet for at least 3 seconds.`}
-                style={{textAlign: 'center', marginTop: 15, lineHeight: 20}}
-                color={Theme.colors.white}
-                ff={Theme.fonts.ThemeFontLight}
-              />
+    <Wrap autoMargin={false} style={styles.mainContainer}>
+      <Image
+        // @ts-ignore
+        source={getImgSource(Images?.activateFaucet)}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          opacity: 0.7,
+        }}
+        resizeMode="cover"
+        // blurRadius={1}
+      />
+      <Header hasBackButton headerBackgroundType="transparent" />
+      <Wrap autoMargin={false} style={styles.sectionContainer}>
+        <Wrap autoMargin={false} style={styles.section1}>
+          <Wrap autoMargin={false}>
+            <Typography
+              size={22}
+              text={'Diagnostics'}
+              style={{textAlign: 'center', marginTop: 0, lineHeight: 25}}
+              color={Theme.colors.white}
+              ff={Theme.fonts.ThemeFontMedium}
+            />
+            <Typography
+              size={14}
+              text={
+                'Place your hand in front of the \nfaucet for at least 3 seconds.'
+              }
+              style={{textAlign: 'center', marginTop: 15, lineHeight: 20}}
+              color={Theme.colors.white}
+              ff={Theme.fonts.ThemeFontLight}
+            />
 
-              <Typography
-                size={14}
-                text={`Did you see water dispense?`}
-                style={{textAlign: 'center', marginTop: 20, lineHeight: 20}}
-                color={Theme.colors.white}
-                ff={Theme.fonts.ThemeFontLight}
-              />
-            </Wrap>
+            <Typography
+              size={14}
+              text={'Did you see water dispense?'}
+              style={{textAlign: 'center', marginTop: 20, lineHeight: 20}}
+              color={Theme.colors.white}
+              ff={Theme.fonts.ThemeFontLight}
+            />
           </Wrap>
-          <Wrap autoMargin={false} style={styles.section2}>
-            <Wrap autoMargin={false} style={styles.container}>
-              <Row autoMargin={false} style={styles.row}>
-                <Col autoMargin={false} style={[styles.col, {paddingRight: 5}]}>
-                  <Button
-                    type={'secondary'}
-                    title="NO"
-                    onPress={() => {
-                      finishDiagnostics(0);
-                    }}
-                    textStyle={{
-                      fontSize: 10,
-                      fontFamily: Theme.fonts.ThemeFontMedium,
-                    }}
-                  />
-                </Col>
-                <Col autoMargin={false} style={[styles.col, {paddingLeft: 5}]}>
-                  <Button
-                    type={'secondary'}
-                    title="YES"
-                    onPress={() => {
-                      finishDiagnostics(1);
-                    }}
-                    textStyle={{
-                      fontSize: 10,
-                      fontFamily: Theme.fonts.ThemeFontMedium,
-                    }}
-                  />
-                </Col>
-              </Row>
-            </Wrap>
+        </Wrap>
+        <Wrap autoMargin={false} style={styles.section2}>
+          <Wrap autoMargin={false} style={styles.container}>
+            <Row autoMargin={false} style={styles.row}>
+              <Col autoMargin={false} style={[styles.col, {paddingRight: 5}]}>
+                <Button
+                  type={'secondary'}
+                  title="NO"
+                  onPress={() => {
+                    finishDiagnostics(0);
+                  }}
+                  textStyle={{
+                    fontSize: 10,
+                    fontFamily: Theme.fonts.ThemeFontMedium,
+                  }}
+                />
+              </Col>
+              <Col autoMargin={false} style={[styles.col, {paddingLeft: 5}]}>
+                <Button
+                  type={'secondary'}
+                  title="YES"
+                  onPress={() => {
+                    finishDiagnostics(1);
+                  }}
+                  textStyle={{
+                    fontSize: 10,
+                    fontFamily: Theme.fonts.ThemeFontMedium,
+                  }}
+                />
+              </Col>
+            </Row>
           </Wrap>
         </Wrap>
       </Wrap>
-    </AppContainer>
+    </Wrap>
   );
 };
 
