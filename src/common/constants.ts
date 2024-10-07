c032980d8765a66d89e9bfba9da949f1cc9fe28e
Deepak Maurya <deepakmaurya@hotmail.com>
2024-10-08 05:24:00 +0530

    Fixed the ios build issue with xocde 16 and mac 15

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -34,12 +34,12 @@ const BASE_DATE_TIME_FROM_FORMAT = 'YYYY-MM-DD HH:mm';
 
 const ANDROID_APP_LINK =
   'https://play.google.com/store/apps/details?id=app.appID&pli=1';
-const ANDROID_APP_VERSION = '1.2.5';
-const ANDROID_RELEASE_DATE = 'Sep 19, 2024';
+const ANDROID_APP_VERSION = '1.2.6';
+const ANDROID_RELEASE_DATE = 'Oct 02, 2024';
 const IOS_APP_LINK =
   'https://play.google.com/store/apps/details?id=app.appID&pli=1';
-const IOS_APP_VERSION = '1.2.5';
-const IOS_APP_RELEASE_DATE = 'Sep 19, 2024';
+const IOS_APP_VERSION = '1.2.6';
+const IOS_APP_RELEASE_DATE = 'Oct 02, 2024';
 const RELEASE_DATE = isAndroid ? ANDROID_RELEASE_DATE : IOS_APP_RELEASE_DATE;
 const APP_VERSION = isAndroid ? ANDROID_APP_VERSION : IOS_APP_VERSION;
 const RELEASE_TEXT = `Release On: ${
